aa85f8ced103eb4c5a708ec9440a9bad07aaf6fb
Iswarya Parakala <113782656+Iswarya1404@users.noreply.github.com>
2023-03-06 20:40:22 +0530

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,7 @@
+#program for math tables
+n = int(input("enter any number:"))
+print("MULTIPLICATION TABLE OF",n)
+print("-----------------------")
+for i in range(1,11):
+    print(n, "x" ,i, "=" , n*i)
+
